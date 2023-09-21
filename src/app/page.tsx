7638b4752a05c0b1bb46afdbf24050bf75ab91c5
Mudit Khanna <muditkhanna998@gmail.com>
2023-09-21 18:05:10 -0400

    changes resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,7 +94,7 @@ export default function Home() {
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500
                  text-white px-4 py-2 rounded-md ml-8"
-                  href="/Mudit-Khanna-Resume.pdf"
+                  href="/Mudit-Resume.pdf"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
